1cb736fcceacbfbff408075725d9a23fa8f538ba
YOUSEF YAGOUB YOUSEF ALSULIMAN <443105666@student.ksu.edu.sa>
2025-06-01 21:46:37 +0300

    Add files via upload

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +28,7 @@ SECRET_KEY = "django-insecure-71h18vo+in4qv#%v^8$l!d(jfv(^$5!=sil&&83f7pnt)mfq)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
